7fbee2ac5675d978c0104e5c3bdb8a828ce58ba2
kayanhendges <kayanbhendges@gmail.com>
2022-10-31 22:43:25 -0300

    feat: create repositories folders

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProfessionalsModule } from './modules/professionals/professionals.module';
+import { ProfissionalsController } from './modules/profissionals/profissionals.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [ProfessionalsModule],
+  controllers: [AppController, ProfissionalsController],
   providers: [AppService],
 })
 export class AppModule {}
